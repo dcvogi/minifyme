ffde534f256c484e121a6537661bd5345d4862f7
dcvogi <dcvogi@gmail.com>
2019-04-21 18:11:09 +0300

    Datastore to Firestore migration. Base62 minification

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 
 //Needed for Firestore
-var {Datastore} = require('@google-cloud/datastore');
-const datastoreProjectId = 'minify-me-20190315';
-const datastore = new Datastore({
-    projectId: datastoreProjectId,
+var {Firestore} = require('@google-cloud/firestore');
+const config = require('../config.json');
+const firestoreProjectId = config.firestoreProjectId;
+const firestore = new Firestore({
+    projectId: firestoreProjectId,
 });
 
 //Needed to handle POST requests 
@@ -12,6 +13,7 @@ const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({
     extended: false
 });
+const base62 = require("base62/lib/ascii");
 
 var router = express.Router();
 
@@ -27,47 +29,38 @@ router.get('/', function (req, res, next) {
 /* POST home page. */
 router.post('/', urlencodedParser, function (req, res, next) {
     async function quickStart() {
-        const statsKey = datastore.key(["stats", "basic_stats"]);
-        // Get number of entities
-        const [stats] = await datastore.get(statsKey);
-        // The kind for the new entity
-        const kind = 'shortenedURLS';
-        // The name/ID for the new entity
-        const name = makeid();
-        // The Cloud Datastore key for the new entity
-        const taskKey = datastore.key([kind, name]);
-
-        const query = datastore.createQuery('shortenedURLS').filter("urlInserted", "=", req.body.urlToMinify).order('urlInserted').order('dateCreated').limit(1)
-        const [exists] = await datastore.runQuery(query);
-        //Check if sent empty TODO
-        if (exists.length) {
+        
+        const statsDocument = firestore.doc(`stats/basic_stats`);
+        // Query if url exists
+        const query = firestore.collection('shortenedURLS').where("urlInserted", "=", req.body.urlToMinify).limit(1);
+        const exists = await query.get();
+    
+        if (exists._size) {
             res.json({
-                'id': exists[0][datastore.KEY].name,
-                'url': exists[0].urlInserted,
+                'id': exists.docs[0].id,
+                'url': exists.docs[0].data().urlInserted,
             });
         }
         else {
-             //Prepares the new entity
-            const task = {
-                key: taskKey,
-                data: {
-                    dateCreated: new Date(),
-                    urlInserted: req.body.urlToMinify,
-                },
-            };
-
-            stats.totalURLs++
-            //Saves the entity
-            await datastore.update({
-                key: statsKey,
-                data: stats
+            var stats = await statsDocument.get()
+            const updatedEntities = Number(stats.data().totalURLs) + 1
+            // The name/ID for the new entity
+            const name = makeid(updatedEntities);
+            // The Cloud Firestore key for the new entity
+            const newDocument = firestore.doc(`shortenedURLS/${name}`);
+            newDocument.set({
+                dateCreated: new Date(),
+                urlInserted: req.body.urlToMinify,
             });
-            await datastore.save(task);
-            console.log(`Sortened to ${task.key.name} from ${task.data.urlInserted}`);
+
+            //Saves the entity
+            await statsDocument.update({
+                totalURLs: updatedEntities
+            });
 
             res.json({
-                'id': task.key.name,
-                'url': task.data.urlInserted,
+                'id': name,
+                'url': req.body.urlToMinify,
             });
         }
     }
@@ -77,34 +70,21 @@ router.post('/', urlencodedParser, function (req, res, next) {
 router.get('/:id', function (req, res) {
     async function getDocument() {
         const kind = 'shortenedURLS';
-        const taskKey = datastore.key([kind, req.params.id]);
+        const taskKey = firestore.key([kind, req.params.id]);
         
-        const [entity] = await datastore.get(taskKey);
+        const [entity] = await firestore.get(taskKey);
         if(entity){
             res.redirect(301, entity.urlInserted);
         }
         else {
             res.redirect(301, "http://www.minify-me.com/error");
         }
-        
-
-        // if( entity.urlInserted.startsWith("http") )
-        //     res.redirect(entity.urlInserted);
-        // else
-        //     res.redirect("http://" + entity.urlInserted);
     }
-    
     getDocument();
 });
 
-function makeid() {
-    var text = "";
-    var possible = "lz3R4hVHtj7K9axOvL8mp6SBYoPkrcINGueWgwU20qbyfTMd5EA1nCJQsZXiDF";
-
-    for (var i = 0; i < 6; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
+function makeid(entityNumber) {
+    return base62.encode(entityNumber);
 }
 
 module.exports = router;
